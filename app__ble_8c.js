492c815598de66e12d4cf83f4326111ae809a6d3
ojousima <otso@ojousima.net>
2022-10-25 13:27:22 +0000

    Deploying to gh-pages from @ ruuvi/ruuvi.gateway_nrf.c@41a7e63d582003ce89f0e39a3dc3682957b927e0 🚀

--- app__ble_8c.js
+++ app__ble_8c.js
@@ -13,7 +13,7 @@ var app__ble_8c =
     [ "RB_BLE_UNKNOWN_MANUFACTURER_ID", "app__ble_8c.html#a5e665eae173e9f0da537a8e478bc8762", null ],
     [ "app_ble_channels_get", "app__ble_8c.html#a9037e9e1fca9f7c17951c9a525002cfa", null ],
     [ "app_ble_channels_set", "app__ble_8c.html#a79dafbe6b4e37f9d8ecec1f6a49c8792", null ],
-    [ "app_ble_manufacturer_filter_enabled", "app__ble_8c.html#ae87afba7d57c76b96c9ab6d6204eb4d2", null ],
+    [ "app_ble_manufacturer_filter_enabled", "app__ble_8c.html#a28b890e850ebceb2e88c7e0335124779", null ],
     [ "app_ble_manufacturer_filter_set", "app__ble_8c.html#a149b265971b2922c47e86dd9ac3ae7f9", null ],
     [ "app_ble_manufacturer_id_set", "app__ble_8c.html#a2f054342f943f1ffacdb330ff88280f6", null ],
     [ "app_ble_modulation_enable", "app__ble_8c.html#a7e5db8051b8b396c54687e48c0cd283c", null ],
--- app__ble_8h.js
+++ app__ble_8h.js
@@ -3,7 +3,7 @@ var app__ble_8h =
     [ "app_ble_scan_t", "structapp__ble__scan__t.html", "structapp__ble__scan__t" ],
     [ "app_ble_channels_get", "app__ble_8h.html#a9037e9e1fca9f7c17951c9a525002cfa", null ],
     [ "app_ble_channels_set", "app__ble_8h.html#a79dafbe6b4e37f9d8ecec1f6a49c8792", null ],
-    [ "app_ble_manufacturer_filter_enabled", "app__ble_8h.html#ae87afba7d57c76b96c9ab6d6204eb4d2", null ],
+    [ "app_ble_manufacturer_filter_enabled", "app__ble_8h.html#a28b890e850ebceb2e88c7e0335124779", null ],
     [ "app_ble_manufacturer_filter_set", "app__ble_8h.html#a149b265971b2922c47e86dd9ac3ae7f9", null ],
     [ "app_ble_manufacturer_id_set", "app__ble_8h.html#a2f054342f943f1ffacdb330ff88280f6", null ],
     [ "app_ble_modulation_enable", "app__ble_8h.html#a7e5db8051b8b396c54687e48c0cd283c", null ],
