2741d70af32bea9efa5027de3fd081416fe16930
TheSomeMan <AlexeyN.Skvortsov@gmail.com>
2023-10-16 11:41:57 +0000

    Deploying to gh-pages from @ ruuvi/ruuvi.gateway_nrf.c@ab434112031f41ec72f1de4eb2ff3fc26c03ea60 🚀

--- search/all_4.js
+++ search/all_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['nrf5_5fsdk15_5fapp_5fconfig_2eh_37',['nrf5_sdk15_app_config.h',['../nrf5__sdk15__app__config_8h.html',1,'']]],
-  ['nrf5_5fsdk15_5fconfigured_38',['NRF5_SDK15_CONFIGURED',['../nrf5__sdk15__app__config_8h.html#a78d2fe783ce2c2843dd058adef0f5584',1,'nrf5_sdk15_app_config.h']]],
-  ['nrf_20sdk15_20configuration_39',['nRF SDK15 Configuration',['../group___s_d_k15.html',1,'']]]
+  ['nrf_20sdk15_20configuration_37',['nRF SDK15 Configuration',['../group___s_d_k15.html',1,'']]],
+  ['nrf5_5fsdk15_5fapp_5fconfig_2eh_38',['nrf5_sdk15_app_config.h',['../nrf5__sdk15__app__config_8h.html',1,'']]],
+  ['nrf5_5fsdk15_5fconfigured_39',['NRF5_SDK15_CONFIGURED',['../nrf5__sdk15__app__config_8h.html#a78d2fe783ce2c2843dd058adef0f5584',1,'nrf5_sdk15_app_config.h']]]
 ];
--- search/groups_0.js
+++ search/groups_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['application_20configuration_118',['Application configuration',['../group__app__config.html',1,'']]],
-  ['application_20uart_20functionality_2e_119',['Application UART functionality.',['../group___a_p_p___u_a_r_t.html',1,'']]],
-  ['application_20configuration_2e_120',['Application configuration.',['../group___configuration.html',1,'']]]
+  ['application_20configuration_2e_119',['Application configuration.',['../group___configuration.html',1,'']]],
+  ['application_20uart_20functionality_2e_120',['Application UART functionality.',['../group___a_p_p___u_a_r_t.html',1,'']]]
 ];
